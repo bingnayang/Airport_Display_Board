fe79e7cb9f8207fff458283ae9f5f428c3f7d9af
BingNa Yang <bingnayang@gmail.com>
2020-11-20 18:57:50 -0500

    refactoring departures rest controller to use service layer and create get departure method by Id

--- displayboard-backend/src/main/java/com/airport/displayboardbackend/rest/DeparturesRestController.java
+++ displayboard-backend/src/main/java/com/airport/displayboardbackend/rest/DeparturesRestController.java
@@ -4,26 +4,40 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.airport.displayboardbackend.dao.DeparturesDAO;
 import com.airport.displayboardbackend.entity.Departures;
+import com.airport.displayboardbackend.service.DepartureService;
 
 @RestController
 @RequestMapping("/api")
 public class DeparturesRestController {
 
-		private DeparturesDAO departuresDAO;
+		private DepartureService departureService;
 		
-		// Inject departures dao by constructor injection
+		// Inject departures service by constructor injection
 		@Autowired
-		public DeparturesRestController(DeparturesDAO theDeparturesDAO) {
-			departuresDAO = theDeparturesDAO;
+		public DeparturesRestController(DepartureService theDepartureService) {
+			departureService = theDepartureService;
 		}
 		
 		@GetMapping("/departures")
 		public List<Departures> findAll(){
-			return departuresDAO.findAll();
+			return departureService.findAll();
+		}
+		
+		
+		@GetMapping("/departures/{departureId}")
+		public Departures getDeparture(@PathVariable int departureId) {
+			Departures theDeparture = departureService.findById(departureId);
+			
+			if(theDeparture == null) {
+				throw new RuntimeException("Departure Id not found - "+departureId);
+			}
+			
+			return theDeparture;
 		}
 }
