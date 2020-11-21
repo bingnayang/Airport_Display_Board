ea516fc4633e77655985cc860815508cb25421d6
BingNa Yang <bingnayang@gmail.com>
2020-11-21 15:58:09 -0500

    install bootstrap, and create departure component and class

---
+++ displayboard-frontend/src/app/departure-board.spec.ts
@@ -0,0 +1,7 @@
+import { DepartureBoard } from './departure-board';
+
+describe('DepartureBoard', () => {
+  it('should create an instance', () => {
+    expect(new DepartureBoard()).toBeTruthy();
+  });
+});
---
+++ displayboard-frontend/src/app/departure-board.ts
@@ -0,0 +1,10 @@
+export class DepartureBoard {
+    id: number;
+    destination: string;
+    time: string;
+    gate: string;
+    status: string;
+    airline: string;
+    flight: string;
+
+}
