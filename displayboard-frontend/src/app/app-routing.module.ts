9f3d2a485c0e6c3334cc027a1f37ed458b844f15
BingNa Yang <bingnayang@gmail.com>
2020-11-22 20:45:37 -0500

    fixed update routing and navigation

--- displayboard-frontend/src/app/app-routing.module.ts
+++ displayboard-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartureListComponent } from './departure-list/departure-list.component';
 import { CreateDepartureComponent } from './create-departure/create-departure.component';
+import { ArrivalListComponent } from './arrival-list/arrival-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'app',pathMatch:'full'},
   {path: 'departures', component: DepartureListComponent},
+  {path: 'arrivals', component: ArrivalListComponent},
   {path: 'create-departure', component: CreateDepartureComponent}
 ];
 
