315037369b95b541f9750ef9a6ac4589a39262fa
BingNa Yang <bingnayang@gmail.com>
2020-11-25 14:57:31 -0500

    connecting anglar with GET arrival REST API

--- displayboard-frontend/src/app/update-arrival/update-arrival.component.ts
+++ displayboard-frontend/src/app/update-arrival/update-arrival.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArrivalBoard } from '../arrival-board';
+import { ArrivalBoardService } from '../arrival-board.service';
 
 @Component({
   selector: 'app-update-arrival',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-arrival.component.css']
 })
 export class UpdateArrivalComponent implements OnInit {
-  
-  constructor() { }
+  id: number;
+  arrival: ArrivalBoard = new ArrivalBoard();
+
+  constructor(private arrivalService: ArrivalBoardService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.arrivalService.getArrivalById(this.id).subscribe(data => {
+      this.arrival = data;
+    },error => console.log(error));
   }
 
 }
